4666fb02230d49cca1dcdaa8da62db4540e81ac1
richardyang <richardyang92@icloud.com>
2024-03-28 16:18:57 +0800

    add command params parser

--- fashion_mnist/src/main.rs
+++ fashion_mnist/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use burn::{backend::{wgpu::{AutoGraphicsApi, WgpuDevice}, Autodiff, Wgpu}, data::dataset::Dataset, optim::AdamConfig};
 
 use crate::{data::FashionMNISTDataset, model::ModelConfig, training::TrainingConfig};
@@ -11,13 +13,19 @@ fn main() {
     type MyBackend = Wgpu<AutoGraphicsApi, f32, i32>;
     type MyAutodiffBackend = Autodiff<MyBackend>;
 
+    let args = env::args().collect::<Vec<String>>();
+
     let device = WgpuDevice::BestAvailable;
-    training::train::<MyAutodiffBackend>(
-        "./fashion_mnist/model",
-        TrainingConfig::new(ModelConfig::new(10, 64 * 5 * 5), AdamConfig::new()),
-        device,
-    );
-    // let dataset = FashionMNISTDataset::test();
-    // let item = dataset.get(100).unwrap();
-    // inference::infer::<MyAutodiffBackend>("./model", device, item);
+
+    if args[1].eq("train") {
+        training::train::<MyAutodiffBackend>(
+            "./fashion_mnist/model",
+            TrainingConfig::new(ModelConfig::new(10, 64 * 5 * 5), AdamConfig::new()),
+            device,
+        );
+    } else {
+        let dataset = FashionMNISTDataset::test();
+        let item = dataset.get(100).unwrap();
+        inference::infer::<MyAutodiffBackend>("./model", device, item);
+    }
 }
